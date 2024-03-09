257a8dd40d70c712d28e3af2467cdc39632ba730
hamedmajdi <hamed90_viva@yahoo.com>
2024-03-09 10:42:29 +0300

    Initial Commit

---
+++ SwiftUI - Flight Ticket Purchase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI - Flight Ticket Purchase
+//
+//  Created by Hamed Majdi on 3/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
