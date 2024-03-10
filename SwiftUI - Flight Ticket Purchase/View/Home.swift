4e237a37c10da0f709d2b4f0b38476287225a65c
hamedmajdi <hamed90_viva@yahoo.com>
2024-03-10 11:59:24 +0300

    fixed some bugs and applied the add button to main screen

--- SwiftUI - Flight Ticket Purchase/View/Home.swift
+++ SwiftUI - Flight Ticket Purchase/View/Home.swift
@@ -19,9 +19,31 @@ struct Home: View {
     var body: some View {
         VStack(spacing: 0){
             HeaderView()
+                .overlay(alignment: .bottomTrailing){
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "plus")
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                            .foregroundStyle(.gray)
+                            .frame(width: 40, height: 40)
+                            .background(
+                                Circle()
+                                    .fill(.white)
+                                    .shadow(color: .black.opacity(0.35), radius: 5, x: 5, y: 5)
+                            )
+                    }
+                    .offset(x: -15, y: 15)
+                }
                 .zIndex(1)
             PayementCardsView()
                 .zIndex(0)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background{
+            Color("BG")
+                .ignoresSafeArea()
         }
     }
     
@@ -68,27 +90,6 @@ struct Home: View {
                     startPoint: .top,
                     endPoint: .bottom))
         }
-    }
-    
-    ///Flight Details
-    @ViewBuilder
-    func FlightDetailView(alignment: HorizontalAlignment = .leading, place: String, code: String, timing: String) -> some View{
-        
-        VStack(alignment: alignment, spacing: 6){
-            Text(place)
-                .font(.caption)
-                .foregroundStyle(.white.opacity(0.8))
-            
-            Text(code)
-                .font(.title)
-                .foregroundStyle(.white)
-            
-            Text(timing)
-                .font(.caption)
-                .foregroundStyle(.white.opacity(0.8))
-        }
-        .frame(maxWidth: .infinity)
-        
     }
     
     @ViewBuilder
@@ -166,6 +167,10 @@ struct Home: View {
                     }
                 })
         )
+        .background{
+            Color.white
+                .ignoresSafeArea()
+        }
     }
     
     @ViewBuilder
@@ -195,4 +200,202 @@ struct Home: View {
 
 #Preview {
     ContentView()
+}
+
+
+/// Flight Details
+struct FlightDetailView: View {
+    var alignment: HorizontalAlignment = .leading
+    var place: String
+    var code: String
+    var timing: String
+    
+    var body: some View {
+        
+        VStack(alignment: alignment, spacing: 6){
+            Text(place)
+                .font(.caption)
+                .foregroundStyle(.white.opacity(0.8))
+            
+            Text(code)
+                .font(.title)
+                .foregroundStyle(.white)
+            
+            Text(timing)
+                .font(.caption)
+                .foregroundStyle(.white.opacity(0.8))
+        }
+        .frame(maxWidth: .infinity)
+    }
+}
+
+// Detail View
+// TODO: Move this view to it's own file
+struct DetailView: View {
+    
+    var size: CGSize
+    var safeArea: EdgeInsets
+    
+    var body: some View {
+        VStack{
+            VStack{
+                VStack(spacing: 0){
+                    Image("logo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 35)
+                    
+                    Text("Your order was submitted")
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.white)
+                        .padding(.top, 20)
+                    
+                    Text("We are waiting for booking confirmation")
+                        .font(.caption)
+                        .foregroundStyle(.white.opacity(0.8))
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.top, 30)
+                .padding(.bottom, 40)
+                .background{
+                    RoundedRectangle(cornerRadius: 15, style: .continuous)
+                        .fill(.white.opacity(0.1))
+                }
+                
+                HStack{
+                    FlightDetailView(place: "Istanbul", code: "IST", timing: "20 Feb, 14:30")
+                    
+                    VStack(spacing: 8){
+                        Image(systemName: "chevron.right")
+                            .font(.title2)
+                        
+                        Text("13H 30m")
+                    }
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.white)
+                    .frame(maxWidth: .infinity)
+                    FlightDetailView(place: "Los Angles", code: "LAX", timing: "21 Feb, 04:00")
+
+                }
+                .padding(15)
+                .padding(.bottom, 70)
+                .background{
+                    RoundedRectangle(cornerRadius: 15, style: .continuous)
+                        .fill(.ultraThinMaterial)
+                }
+                .padding(.top, -20)
+            }
+            .padding(.horizontal, 20)
+            .padding(.top, safeArea.top + 15)
+            .padding([.horizontal, .bottom], 15)
+            .background{
+                Rectangle()
+                    .fill(Color("BlueTop"))
+                    .padding(.bottom, 90)
+            }
+            
+            //Contact Information View
+            GeometryReader{proxy in
+                ViewThatFits{
+                    ContactInformation()
+                    ScrollView(.vertical, showsIndicators: false){
+                        ContactInformation()
+                    }
+                }
+            }
+        }
+        
+    }
+    
+    @ViewBuilder
+    func ContactInformation() -> some View{
+        VStack(spacing: 15){
+            HStack{
+                InfoView(title: "Flight", subtitle: "CA 1549")
+                InfoView(title: "Class", subtitle: "Premium")
+                InfoView(title: "Aircraft", subtitle: "B 737-Max9")
+                InfoView(title: "Possibility", subtitle: "CA 1549")
+            }
+            
+            ContactView(name: "Hamed Majdi", email: "majdi.haamed@gmail.com", profile: "user1")
+                .padding(.top, 30)
+            
+            ContactView(name: "Craig Federighi", email: "craig.fed@gmail.com", profile: "user2")
+                .padding(.top, 10)
+            
+            VStack(alignment: .leading, spacing: 4){
+                Text("Total Price")
+                    .font(.caption)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.gray)
+                
+                Text("$1,367.00")
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.black)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.top, 20)
+            .padding(.leading, 15)
+            
+            
+            
+            // Home Screen Button
+            Button{
+                
+            } label: {
+                Text("Go to Home Screen")
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.white)
+                    .padding(.horizontal, 30)
+                    .padding(.vertical, 10)
+                    .background{
+                            Capsule()
+                            .fill(Color("BlueTop").gradient)
+                    }
+            }
+            .padding(.top, 15)
+            .frame(maxHeight: .infinity, alignment: .bottom)
+            .padding(.bottom, safeArea.bottom)
+        }
+        .padding(15)
+    }
+    
+    @ViewBuilder
+    func ContactView(name: String, email: String, profile: String) -> some View{
+        HStack{
+            VStack(alignment: .leading, spacing: 4){
+                Text(name)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.black)
+                
+                Text(email)
+                    .font(.callout)
+                    .foregroundStyle(.gray)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Image(profile)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 45, height: 45)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+        }
+        .padding(.horizontal, 15)
+    }
+    
+    @ViewBuilder
+    func InfoView(title: String, subtitle: String) -> some View{
+        VStack(alignment: .center, spacing: 4){
+            Text(title)
+                .font(.caption2)
+                .fontWeight(.semibold)
+                .foregroundStyle(.gray)
+            
+            Text(subtitle)
+                .font(.caption)
+                .fontWeight(.semibold)
+                .foregroundStyle(.gray)
+        }
+        .frame(maxWidth: .infinity)
+    }
 }
