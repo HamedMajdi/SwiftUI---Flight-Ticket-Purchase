c6d6020835967c0716bada2c00de4631349a361c
hamedmajdi <hamed90_viva@yahoo.com>
2024-03-11 22:50:24 +0300

    completed the ring animation

---
+++ SwiftUI - Flight Ticket Purchase/Model/PayementStatus.swift
@@ -0,0 +1,18 @@
+//
+//  PayementStatus.swift
+//  SwiftUI - Flight Ticket Purchase
+//
+//  Created by Hamed Majdi on 3/10/24.
+//
+
+import SwiftUI
+
+struct PayementStatus: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PayementStatus()
+}
